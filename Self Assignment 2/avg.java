6550677b02008a25a29292e02a134b2de7cc334a
Aishwary Panchal <aishwary.panchal001@stud.fh-dortmund.de>
2025-09-25 12:48:48 +0200

    Modified and completed first pass of AGV code

--- Self Assignment 2/avg.java
+++ Self Assignment 2/avg.java
@@ -6,72 +6,90 @@ package SA2;
 public class avg {
 	
 	private String id;
-	private double maxspeed;
-	private double actspeed;
-	private double consumptionRate;
-	private double chargeTime; //Time
-	private double batteryload; 
-	//private double[] pos= {0,0};// initial position (in the warehouse)
+	private float maxspeed; // in km/h
+	private double actspeed; // in km/h
+	private double consumptionRate; // %/h
+	private double chargeTime; // Time in hours
+	private double batteryload; // in %
+	private double[] pos = new double[2]; // For initial position in the warehouse (x,y coordinates)
+	private double overallTime;
+	private double overallConsum;
 	
 	
-	//setter
-	public avg (String type, float maxsp, double comsup)
+	
+	// setter
+	public avg (String type, float maxsp, double comsup) // AVG constructor
 	{
-		id= type;
-		maxspeed=maxsp;
-		consumptionRate=comsup;
-		chargeTime=4; //hours Time
-		actspeed=0;
-		batteryload=100; //100% from start
-		//pos= (0,0);//position
+		id=type;
+		maxspeed=maxsp; // in km/h
+		consumptionRate=comsup; // in %/h
+		chargeTime=4; // time in hours
+		actspeed=0; // Always starting at rest (in km/h)
+		batteryload=100; // 100% from start
+		pos[0]=0; // x-position
+		pos[1]=0; // y-position
 	}
 	
-	public void setActSpeed(double speed)
+	public void setActSpeed(double speed) // Sets the actual speed of the vehicle
 	{
-		actspeed=speed;
+		if (speed <= maxspeed && speed > 0) {
+			actspeed=speed;
+		} else { 
+			actspeed = maxspeed
+		}
 	}
 	
-	public double changepos (double[] p2) // change of position entitles a battery consumption and a distance traveled
+	public double[] changepos (double[] p2) // change of position entitles a battery consumption and a distance traveled
 	{
 		double distance=0;
-		distance = 0;//math
-		batteryload =0;//math
-		//pos=p2; //new position
+		double time = 0;
+		double batteryConsumed = 0;
 		
-		return distance;
+		distance = Math.abs(pos[0] - p2[0]) + Math.abs(pos[1] - p2[1]); // Actual distance travelled along x AND y
+		time = distance / actspeed; // time taken to cover the distance at actual speed
+		batteryConsumed = consumptionRate * time; // Battery consumed in % for the time the AGV runs
+		batteryload = batteryload - batteryConsumed; // remaining battery level
+		pos[0] = p2[0]; // new position of the AGV
+		pos[1] = p2[1];
+		
+		overallTime += time; // calcuating overall time consumed for the Iop by this AGV
+		overallConsum += batteryConsumed // calcuating overall battery consumed for the Iop by this AGV
+		
+		return pos;
 	}
 	
 	//getters
 	
-	public double getmaxSpeed()
+	public double getmaxSpeed() // returns the max speed of the AGV
 	{
 		return maxspeed;
 	}
 	
-	public double getactSpeed()
+	public double getactSpeed() // returns the actual speed of the AGV
 	{
 		return actspeed;
 	}
 	
-	public double getComsup()
+	public double getComsup() // get the overall battery consumption 
+	// Maybe can set an overall battery value here like 1000 Wh that can later be calculated relative to %
 	{
 		return consumptionRate;
 	}
 	
-	public double getchargeTime()
+	public double getchargeTime() // TODO
 	{
 		return chargeTime;
 	}
 	
-	public double getbatteryLoad()
+	public double getbatteryLoad() // returns remaining battery level in the AGV
 	{
 		return batteryload;
 	}
 	
-	/*public double[] getpos()
+	public double[] getpos() // returns current position of the AGV
 	{
 		return pos;
-	}*/
+	}
 	
 	
 	
