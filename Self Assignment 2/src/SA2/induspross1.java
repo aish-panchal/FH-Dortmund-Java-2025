173decc3f02065fa2143595b51938ca0bb852193
Aishwary Panchal <aishwary.panchal001@stud.fh-dortmund.de>
2025-09-26 00:05:15 +0200

    committing the Java project folder with the source code

---
+++ Self Assignment 2/src/SA2/induspross1.java
@@ -0,0 +1,50 @@
+package SA2;
+
+public class induspross1 {
+	
+	private String idpross;
+	//private String ops[]; //Operations required for this Industrial Process
+	
+	
+	public induspross1 ()
+	{
+
+		idpross = "First industrial process";
+	/*	ip1_ops1 = new iOp1();
+		ip1_ops2 = new iOp2();
+		ip1_ops3 = new iOp3();
+		
+		ops[0]= ip1_ops1.getAll;
+		*/
+	}
+	
+	public double processDuration()
+	{
+		double indpro1_duration=0;
+		//math
+		return indpro1_duration;
+	}
+	
+	public int processResources()
+	{
+		int avgsused=0;
+	/*
+		for(int i=0;i<=ops.length;i++)
+		{
+			avgsused = avgsused + ops[i].getavgUsed(); // sums the total avg used per operation in the process
+		}
+	*/	
+		return avgsused;
+	}
+	
+	/*
+	
+	//getter
+	public void processData()
+	{
+	
+		System.out.println("Number of AVGs used: " + avgsused);
+	}
+	
+*/
+}
