5f5d9560e64565b3b0c92ba3ccadf1016313c6bc
yo_boi <abdelrahmanbbb@gmail.com>
2025-10-04 21:46:30 +0200

    added methods for humanresource

--- Self Assignment 3/src/SA3/HumanResource.java
+++ Self Assignment 3/src/SA3/HumanResource.java
@@ -3,6 +3,18 @@ package SA3;
 public class HumanResource extends Resource{
     public String name;
     public String position;
+    private double hours_worked;
+    public double salary;
 
-    // add methods
+    public HumanResource(String name, String position, double salary){
+	this.name = name;
+	this.position = position;
+	this.salary = salary;
+    }
+    public void work(double hours){
+	hours_worked += hours;
+    }
+    public double payable_salary(){
+	return hours_worked * salary;
+    }
 }
