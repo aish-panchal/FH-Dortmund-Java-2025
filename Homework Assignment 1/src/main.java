c7df623b5094f17f6db8147dd52e6c998b9f2650
Girsy Guzmán <girsyd@gmail.com>
2025-10-27 20:44:00 +0100

    Added main with implementation of task management

---
+++ Homework Assignment 1/src/main.java
@@ -0,0 +1,22 @@
+package HA1;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*Type of tasks: {"toFactory", "toWarehouse","toDelivery","toCharge"}
+ * */
+public class main {
+	
+	public static void main (String[] args) {
+		
+		Date today = new java.util.Date();
+		int day=0;
+
+		taskManagement workday = new taskManagement(today); //array of days //depending on number of days should archive/delete files
+		
+		workday.takeOrder(50, "toFactory");//Order of 50 tones from warehouse to factory
+		workday.takeOrder(100, "toWarehouse");
+		workday.takeOrder(200, "toDelivery");
+	}
+
+}
