81669c06a7db8b862342e91425e8c747cca27739
yo_boi <abdelrahmanbbb@gmail.com>
2025-10-29 01:04:38 +0100

    added id to raw materials

--- Homework Assignment 1/src/rawMaterial.java
+++ Homework Assignment 1/src/rawMaterial.java
@@ -1,8 +1,10 @@
 public class rawMaterial{
+    public String id;
     public String type;
     public int amount;
     public double[] location = new double[2];
-    public rawMaterial(String type, int amount, double[] location){
+    public rawMaterial(String id, String type, int amount, double[] location){
+	this.id = id;
 	this.type = type;
 	this.amount = amount;
 	this.location = location;
