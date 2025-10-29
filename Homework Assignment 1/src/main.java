6c37fb1b9d4d667303d645166238a3548120d220
Girsy Guzman <girsyd@gmail.com>
2025-10-29 20:43:29 +0100

    small changes added

--- Homework Assignment 1/src/main.java
+++ Homework Assignment 1/src/main.java
@@ -8,13 +8,15 @@ public class main {
     public static void main (String[] args) {
 		
 	Date today = new java.util.Date();
-	//int day=0;
 
 	taskManagement workday = new taskManagement(today); //array of days //depending on number of days should archive/delete files
 		
-	//workday.takeOrder(50, "toFactory");//Order of 50 tones from warehouse to factory
-	//workday.takeOrder(100, "toWarehouse");
+	workday.takeOrder(50, "toFactory");//Order of 50 tones from warehouse to factory
+	workday.takeOrder(100, "toWarehouse");
 	workday.takeOrder(50, "toDelivery");
+	
+	
+	
     }
 
 }
