c19e493067dbd10e385e4197b7f07cf740b92ea1
yo_boi <abdelrahmanbbb@gmail.com>
2025-10-29 01:04:57 +0100

    expanded storage system capabilities

--- Homework Assignment 1/src/storageEquipment.java
+++ Homework Assignment 1/src/storageEquipment.java
@@ -1,11 +1,26 @@
-
+// represents a shelf or some kind of container
 public class storageEquipment{
-    private double[] pos = new double[2];
+    private double[] location = new double[2];
+    private boolean occupied;
 
+    public storageEquipment(double[] location){
+	this.location = location;
+	this.occupied = false;
+    }
+    public void load(){
+	occupied = true;
+    }
+    public void unload(){
+	occupied = false;
+    }
+    public boolean is_occupied(){
+	// returns true if occupied
+	return occupied;
+    }
     public double[] equipmentLocation(){
-	return pos;
+	return location;
     }
     public void setLocation(double[] location){
-	pos = location;
+	this.location = location;
     }
 }
--- Homework Assignment 1/src/storageManagement.java
+++ Homework Assignment 1/src/storageManagement.java
@@ -1,12 +1,69 @@
+import java.util.ArrayList;
+
 public class storageManagement{
     // change to private later
     public storageEquipment[] equipment;
-    public rawMaterial[] materials;
+    public ArrayList<rawMaterial> stored_materials;
     public storageEquipment[] equipment(){
 	return equipment;
     }
-    public rawMaterial[] materials(){
-	return materials;
+    public ArrayList<rawMaterial> stored_materials(){
+	return stored_materials;
+    }
+    public double[] free_space() throws Exception{
+	// return the location of a free storage space
+	for(storageEquipment s: equipment){
+	    if (s.is_occupied() == false){
+		return s.equipmentLocation();
+	    }	 
+	}
+	throw new Exception();
+	// TODO change to actual "no free space" exception
+    }
+    //TODO make storage exceptions and specific ones for each error
+    public rawMaterial retrieve_material(double[] location) throws Exception{
+	for(rawMaterial item: stored_materials){
+	    if(item.location == location){
+		stored_materials.remove(item);
+		// TODO add logging here
+		return item;
+	    }
+	}
+	throw new Exception();
+    }
+    public rawMaterial retrieve_material(String id) throws Exception{
+	for(rawMaterial item: stored_materials){
+	    if(item.id == id){
+		stored_materials.remove(item);
+		// TODO add logging here
+		return item;
+	    }
+	}
+	throw new Exception();
     }
     
+    public void store_material(rawMaterial item, double[] storage_location) throws Exception{
+	// store a material at a
+	storageEquipment storageSpace = null;
+	for(storageEquipment s: equipment){
+	    if(s.equipmentLocation() == storage_location){
+		if(s.is_occupied()){
+		    throw new Exception();
+		    // space is not empty
+		}
+		storageSpace = s;
+		break;
+	    }
+	}
+	if(null == storageSpace){
+	    throw new Exception();
+	    // no storage space at location
+	}
+	storageSpace.load();
+	item.location = storage_location;
+	stored_materials.addLast(item);
+	// TODO add logging here
+    }
 }
+
+
