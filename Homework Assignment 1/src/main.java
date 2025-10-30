b37f0f360c72d5cd0ce72bed147f17aea1abcc40
Girsy Guzman <girsyd@gmail.com>
2025-10-30 18:09:09 +0100

    Added 'enter file' message

--- Homework Assignment 1/src/main.java
+++ Homework Assignment 1/src/main.java
@@ -1,7 +1,7 @@
 import java.io.Console;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import java.util.Scanner;
 
 /*Type of tasks: {"toFactory", "toWarehouse","toDelivery","toCharge"}
  * */
@@ -10,14 +10,18 @@ public class main {
     public static void main (String[] args) {
 		
 	Date today = new java.util.Date();
-
+	Scanner logname;
+	logname = new Scanner(System.in);
+	
 	taskManagement workday = new taskManagement(today); 
 	workday.takeOrder(50, "toFactory");//Order of 50 tones from warehouse to factory
 	workday.takeOrder(100, "toWarehouse");
 	workday.takeOrder(50, "toDelivery");
 	
-	file_ops.openLog("ChargingStation");
-	
+	while(true) {
+		System.out.println("Enter searching log file: ");
+		file_ops.openLog(logname.nextLine());
+	}
     }
 
 }
