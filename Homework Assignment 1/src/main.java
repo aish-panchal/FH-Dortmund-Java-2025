b547dbf416bce3a14a8a261660b715e7fc5c0063
Girsy Guzman <girsyd@gmail.com>
2025-10-30 17:59:46 +0100

    added 'enter searching file' message

--- Homework Assignment 1/src/main.java
+++ Homework Assignment 1/src/main.java
@@ -1,5 +1,7 @@
+import java.io.Console;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 /*Type of tasks: {"toFactory", "toWarehouse","toDelivery","toCharge"}
  * */
@@ -8,14 +10,18 @@ public class main {
     public static void main (String[] args) {
 		
 	Date today = new java.util.Date();
-
-	taskManagement workday = new taskManagement(today); //array of days //depending on number of days should archive/delete files
-		
+	Scanner logname;
+	logname = new Scanner(System.in);
+	
+	taskManagement workday = new taskManagement(today); 
 	workday.takeOrder(50, "toFactory");//Order of 50 tones from warehouse to factory
-	workday.takeOrder(100, "toWarehouse");
-	workday.takeOrder(50, "toDelivery");
+	workday.takeOrder(100, "toWarehouse"); //Order of 100 tones from factory to warehouse
+	workday.takeOrder(50, "toDelivery");//Order of 50 tones to Delivery
 	
-	
+	while(true) {
+		System.out.println("Enter searching log file: ");
+		file_ops.openLog(logname.nextLine());
+	}
 	
     }
 
