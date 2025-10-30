29fe472d383a7c69151d8271600ca48139e77ee5
Aishwary Panchal <aishwary.panchal001@stud.fh-dortmund.de>
2025-10-30 23:57:20 +0100

    added some exception handler class demos

--- Homework Assignment 2/src/exception_handling.java
+++ Homework Assignment 2/src/exception_handling.java
@@ -1,4 +1,38 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class exception_handling {
-
+	
+	public void multipleExceptions() {
+		try {
+			int[] arr = {1, 2, 3};
+			int num = arr[4];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutofBoundsException demo: " + e.getMessage());
+		} 
+		
+		try {
+			int division = 7 / 0;
+		} catch (ArithmeticException e) {
+			System.out.println("ArithmeticException demo: " + e.getMessage());
+		}
+	}
+	
+	public void rethrowingExceptions() {
+		
+	}
+	
+	public void resourceManagement() {
+		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("testFile.txt"))){
+		    writer.write("Log event: recorded"); 		
+		} catch (IOException e) {
+			System.out.println("IOException demo for resource management: " + e.getMessage());
+		}
+	}
+	
+	public void chainingExceptions() {
+		
+	}
+	
 }
