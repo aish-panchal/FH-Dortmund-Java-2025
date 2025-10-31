7330b03c74cebbcb925535bced099a5087133cfc
yo_boi <abdelrahmanbbb@gmail.com>
2025-10-31 15:32:52 +0100

    added tests for raw material and storage equipment

--- Homework Assignment 2/src/rawMaterialTest.java
+++ Homework Assignment 2/src/rawMaterialTest.java
@@ -1,12 +1,16 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-class rawMaterialtest{
+class rawMaterialTest{
 
     @Test
     void testConstructor(){
 	double[] loc = {1,3};
 	rawMaterial material = new rawMaterial("testID", "gravel", 200, loc);
+	assertEquals("testID", material.id);
+	assertEquals("gravel", material.type);
+	assertEquals(200, material.amount);
+	assertEquals(loc, material.location);
     }
     
 }
