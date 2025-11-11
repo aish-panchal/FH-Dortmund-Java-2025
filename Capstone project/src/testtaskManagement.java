88c2f4c05177ec7ef6de37c71090d465da79fcfb
Girsy Guzman <girsyd@gmail.com>
2025-11-11 21:47:24 +0100

    fixed unit test

--- Capstone project/src/testtaskManagement.java
+++ Capstone project/src/testtaskManagement.java
@@ -4,68 +4,87 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class testtaskManagement {
-	
+
+	Date testdate = new java.util.Date();
+	taskManagement order = new taskManagement(testdate, 25);
+
 	@Test
-	@DisplayName ("Log file creation")
-	void testtaskManager(){
-		Date testdate = new java.util.Date();
-		taskManagement order = new taskManagement(testdate);
-		
-		String testname =("log."+new SimpleDateFormat("yyyy-MM-dd").format(testdate)+".txt");
-		
-		assertEquals(testname,order.file);//the file name follow format "log.yyyy-MM-dd.txt"
+	@DisplayName("Constructor, creates log file and vehicles")
+	void testtaskManager() {
+		taskManagement order1 = new taskManagement(testdate, 25);
+		String testname = ("log." + new SimpleDateFormat("yyyy-MM-dd").format(testdate) + ".txt");
+
+		assertEquals(testname, order1.file);// the file name follow format "log.yyyy-MM-dd.txt"
+		assertEquals(25, order1.vehicles.size()); // there are 25 vehicles available to use
 	}
 	
 	@Test
-	@DisplayName ("Valid load ordered and operating avg amount")
-	void testop_vehicles() throws exception_handling.ZeroTonnesException, exception_handling.InvalidOrderException, exception_handling, exception_handling.VehicleNotFoundException {
-		Date testdate = new java.util.Date();
-		taskManagement order = new taskManagement(testdate);
-		order.takeOrder(10, "toFactory");
+	@DisplayName("Vehicles available after an order")
+	void testavailable_vehicles() throws exception_handling.ZeroTonnesException, exception_handling.InvalidOrderException, exception_handling, exception_handling.VehicleNotFoundException {
+		order.takeOrder(10, "toDelivery");//a low_load need only 5 avgs
 		
-		assertEquals(5,order.move.avgs.length);//a low_load need only 5 avgs
+		int remainingAvgs=20;
+		assertEquals(remainingAvgs, order.vehicles.size());// should remain 20 for other tasks
 	}
 	
 	@Test
-	@DisplayName ("Order with valid name")
-	void testtaskeOrder() throws exception_handling.ZeroTonnesException, exception_handling.InvalidOrderException, exception_handling, exception_handling.VehicleNotFoundException {
+	@DisplayName("Valid load ordered and operating avg amount")
+	void testop_vehicles() throws exception_handling.ZeroTonnesException, exception_handling.InvalidOrderException,
+			exception_handling, exception_handling.VehicleNotFoundException {
+		taskManagement order5 = new taskManagement(testdate,25);
+		order.takeOrder(10, "toFactory");//10 tones only require 5 avg
+		assertEquals(5, order.move.avgsToBeUsed.size());// a low_load need only 5 avgs
+		assertEquals(25, order.vehicles.size());//at the end vehicle shoould return to main avg list
+	//	order.takeOrder(0,"toFactory");//invalid tone order
 		
-		Date testdate = new java.util.Date();
-		taskManagement order = new taskManagement(testdate);
+	}
+
+	@Test
+	@DisplayName("Order with valid name")
+	void testtaskeOrder() throws exception_handling.ZeroTonnesException, exception_handling.InvalidOrderException,
+			exception_handling, exception_handling.VehicleNotFoundException {
+
+		order.takeOrder(10, "toWarehouse");//valid order
+		//order.takeOrder(25, "delivery"); //invalid order
+		order.takeOrder(10, "toDelivery");//valid order
+		int testorderno = 3;//orderno increase after each order
+		
+		assertEquals(testorderno, order.orderno); 
+	}
+
+	@Test
+	@DisplayName("Order managed correctly")
+	void testmanageOrder() throws exception_handling.ZeroTonnesException, exception_handling.InvalidOrderException,
+			exception_handling, exception_handling.VehicleNotFoundException {
+		double warehouse[] = { 3, 1 };
+		double factory[] = { 5, 6 };
+		double dispatch[] = { 4, 5 };
+
 		order.takeOrder(10, "toWarehouse");
 		int testorderno=2;
-		
-		assertEquals(testorderno,order.orderno); //if order was written properly then move is not null
+		assertEquals(testorderno,order.orderno);
+		assertNotNull(order.move);// move object should be initialized after order is managed
 	}
 	
 	@Test
-	@DisplayName ("Order managed correctly")
-	void testmanageOrder() throws exception_handling.ZeroTonnesException, exception_handling.InvalidOrderException, exception_handling, exception_handling.VehicleNotFoundException {
-		double warehouse[]= {3,1};
-	    double factory[]= {5,6};
-	    double dispatch[]= {4,5};
+	@DisplayName("Charging queue after a task")
+	void testcallchargingStation() throws exception_handling.ZeroTonnesException,
+			exception_handling.InvalidOrderException, exception_handling, exception_handling.VehicleNotFoundException {
 		
-	    Date testdate = new java.util.Date();
-		taskManagement order = new taskManagement(testdate);
-		order.takeOrder(10, "toWarehouse");
+		taskManagement order2 = new taskManagement(testdate,25);
+		taskManagement order3 = new taskManagement(testdate,25);
+		order2.takeOrder(10, "toDelivery");//5 vehicles end up in need of charging
+		//order3.takeOrder(10, "toWarehouse");//5 vehicles end up in need of charging
 		
-		assertNotNull(order.move);//move object should be initialized after order is managed
-
+		int avgToCharge=5;
+		
+		assertEquals(avgToCharge,order2.vehiclesInNeedOfCharging.size());// there should be 5 vehicles on the queue
+		assertEquals(0, order3.vehiclesInNeedOfCharging.size());//there should be vehicles in the queue
 	}
-	
-	@Test
-	@DisplayName ("Calling of chargingStation")
-	void testcallchargingStation() throws exception_handling.ZeroTonnesException, exception_handling.InvalidOrderException, exception_handling, exception_handling.VehicleNotFoundException {
-		Date testdate = new java.util.Date();
-		taskManagement order = new taskManagement(testdate);
-		order.takeOrder(10, "toDelivery");
-		
-		assertNotNull(order.charge);//charge object should be initialized after avg ran long process
-	}
-
 }
