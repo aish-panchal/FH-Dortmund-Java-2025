4f7e84b8cad7176cc36c6911ca92473ea2defa8d
Girsy Guzman <girsyd@gmail.com>
2025-11-12 02:31:32 +0100

    fixed logging

--- Capstone project/src/chargingStation.java
+++ Capstone project/src/chargingStation.java
@@ -1,7 +1,6 @@
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 public class chargingStation implements Runnable {
 
@@ -21,92 +20,77 @@ public class chargingStation implements Runnable {
 	public ArrayList<avg> chargingQ;// avg to charge
 	public ArrayList<avg> avg;
 	public ArrayList<pair> charging;
-	
-	//public ArrayList<avg> charging;
-	private boolean stationOccupied; // station status
+
 	public String logf;// charging station file
 	private String taskfile;// system file
 	private String l_event; // event message template
-	private double chargtime; // charging time depends on percentage consumed
 	private Date accTime;// current date
 	private exception_handling echarge = new exception_handling();
 
-	public chargingStation(String taskfile,ArrayList<avg> avgs, ArrayList<avg> toCharge) {
+	public chargingStation(String taskfile, ArrayList<avg> avgs, ArrayList<avg> toCharge) {
 		// initialize log file for all n charging stations
 		accTime = new java.util.Date();
-		this.taskfile= taskfile;
+		this.taskfile = taskfile;
 		logf = (new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(accTime) + "ChargingStation" + ".txt");
 		file_ops.createUpdateLog(logf, "");
 		this.availableStations = STATIONS;
 		this.avg = avgs;
 		this.chargingQ = toCharge;
 		charging = new ArrayList<pair>();
-		//charging = new ArrayList<avg>();
-		l_event=(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(accTime)+ ": Vehicle ");
+		// charging = new ArrayList<avg>();
+		l_event = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(accTime) + ": Vehicle ");
 	}
-	
-	//TODO ----- if all avgs are all unavailable then handle exception, wait blah blah
 
 	@Override
 	public void run() {
-		//System.out.println("In thread");
+		// System.out.println("In thread");
 		String startevent, endevent;
 		accTime = new java.util.Date();
 		while (true) {
 
-			if (chargingQ.size() > 0) {	
+			if (chargingQ.size() > 0) {
+				System.out.println("ChargingQ: " + chargingQ.size());
 				for (int i = 0; (i < chargingQ.size()) && (availableStations > 0); i++) {
 					avg avgToBeCharged = chargingQ.get(0);
-					double chargepercent =avgToBeCharged.getConsump();
+					double chargepercent = avgToBeCharged.getConsump();
 					double chargeTime = avgToBeCharged.chargeBatteryPercentage(chargepercent);
-					
+
 					pair a = new pair(avgToBeCharged, chargeTime);
 					charging.add(a);
-					availableStations-= 1;
-					startevent = (l_event + charging.get(charging.size()-1).avg.id+" is charging.");//avg.id+" is charging.");
-					updateLogFile(charging.get(i).avg.avgfile,startevent);
+					availableStations -= 1;
+					startevent = (l_event + charging.get(charging.size() - 1).avg.id + " is charging.");
+					updateLogFile(charging.get(charging.size() - 1).avg.avgfile, startevent);
 					chargingQ.remove(0);
 					getStationStatus();
 				}
 			}
-		
-			for(int i=0; i<charging.size();i++) {	
+
+			for (int i = 0; i < charging.size(); i++) {
 				charging.get(i).time -= 0.1;
-				System.out.println("time: "+charging.get(i).time);
+
 				if (charging.get(i).time <= 0) {
-					System.out.println("2nd for loop!");
-					charging.get(i).avg.getInfo();
 					avg.add(charging.get(i).avg);
-		
-					endevent = (l_event + charging.get(i).avg.id+" is charged.");
-					updateLogFile(charging.get(i).avg.avgfile,endevent);
+					endevent = (l_event + charging.get(i).avg.id + " is charged.");
+					updateLogFile(charging.get(i).avg.avgfile, endevent);
 					charging.remove(i);
-					availableStations += 1; //update log finish charging
-					getChargingstatus();
+					availableStations += 1; // update log finish charging
+					getStationStatus();
 				}
 				getStationStatus();
 			}
 			try {
 				Thread.sleep(1000);
-				l_event=(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis())+ ": Vehicle ");
+				l_event = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis())
+						+ ": Vehicle ");
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	public String getChargingstatus() {
-		String status;
-		if (stationOccupied) {
-			status = "Vehicle currently charging";
-		} else {
-			status = "Station is available";
-		}
-		return status;
-	}
-
 	public String getStationStatus() {
-		return ("Currently charging: "+charging.size()+ " vehicles.\n\n"+"Stations available: "+(this.availableStations));
+		return ("Currently charging: " + charging.size() + " vehicles.\n\n" + "Stations available: "
+				+ (this.availableStations));
 	}
 
 	public void updateLogFile(String file, String event) {
@@ -114,8 +98,8 @@ public class chargingStation implements Runnable {
 		file_ops.createUpdateLog(file, event);// update vehicle file
 		file_ops.createUpdateLog(logf, event);// update charging station file
 	}
-	
+
 	public String getInfo() {
-    	return "Station Status: " + getStationStatus();
-    }
+		return "Station Status: " + getStationStatus();
+	}
 }
